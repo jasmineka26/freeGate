bebc09a8f7f15a6945d4f29fc645ecbf23107d61
jasmineka26 <149463857+jasmineka26@users.noreply.github.com>
2023-12-25 00:12:41 +0330

    Load background Login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import loadingGif from "../assets/cactus-pavilion.gif";
+import backGroundImage from "../assets/background.jpg";
 import client from "../services/client";
 import Button from "./login/Button";
 import Input from "./login/Input";
@@ -31,40 +32,46 @@ const Login = () => {
 
   return (
     <>
-      <div
-        className={`flex items-center flex-col justify-center h-[590px] w-[460px]
-         bg-opacity-15 bg-white backdrop-blur-md shadow-lg rounded-xl text-white
-         uppercase tracking-wider gap-8
+      <Image
+        src={backGroundImage}
+        className="absolute w-100vh h-100vw bg-cover"
+      />
+      <div className="flex w-screen h-screen justify-center items-center">
+        <div
+          className={`flex items-center flex-col justify-center h-[590px] w-[460px]
+          bg-opacity-15 bg-white backdrop-blur-md shadow-lg rounded-xl text-white
+          uppercase tracking-wider gap-8
          login
      `}
-      >
-        <div className="flex flex-col justify-center items-center gap-3">
-          <div>
-            <Image className="w-28 h-40" src={loadingGif} />
+        >
+          <div className="flex flex-col justify-center items-center gap-3">
+            <div>
+              <Image className="w-28 h-40" src={loadingGif} />
+            </div>
+            <div>
+              <h2>Welcome to Cactus</h2>
+              <h1 className="bg-gradient-to-r from-[#14163c] via-[#03217b] to-[#14163c] w-[100%] h-1 " />
+            </div>
           </div>
-          <div>
-            <h2>Welcome to Cactus</h2>
-            <h1 className="bg-gradient-to-r from-[#14163c] via-[#03217b] to-[#14163c] w-[100%] h-1 " />
+
+          <div className="flex flex-col justify-around items-center h-32 w-full ">
+            <Input
+              type="text"
+              placeholder="نام کاربری"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              type="password"
+              placeholder="رمز ورود"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
-        </div>
 
-        <div className="flex flex-col justify-around items-center h-32 w-full ">
-          <Input
-            type="text"
-            placeholder="نام کاربری"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <Input
-            type="password"
-            placeholder="رمز ورود"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-
-        <div className="flex items-center justify-center w-[60%]">
-          <Button content="Log In" onClick={printUserInfo} />
+          <div className="flex items-center justify-center w-[60%]">
+            <Button content="Log In" onClick={printUserInfo} />
+          </div>
         </div>
       </div>
     </>
