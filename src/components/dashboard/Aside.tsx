0a29b4cad4183155f6cf1b83256127ed1f2848ad
jasmineka26 <149463857+jasmineka26@users.noreply.github.com>
2023-12-26 23:55:29 +0330

    Improving aside UI

--- src/components/dashboard/Aside.tsx
+++ src/components/dashboard/Aside.tsx
@@ -40,7 +40,7 @@ const Aside = () => {
 
   return (
     <aside
-      className={`bg-gradient-to-br from-gray-800 to-gray-900 -translate-x-80  inset-0 z-50 my-4 mr-4 h-[calc(100vh-32px)]
+      className={`bg-gradient-to-br from-gray-800 to-gray-900  inset-0 z-50 my-4 mr-4 h-[calc(100vh-32px)]
      w-72 rounded-xl transition-transform duration-300 xl:translate-x-0`}
     >
       <div className="relative border-b border-white/20">
