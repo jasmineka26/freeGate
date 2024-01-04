f2eb48ae88bc6bb11954e58d256a556a9071f327
jasmineka26 <kadivaryasaman@gmail.com>
2024-01-04 13:00:50 +0330

    Improve UI Report payment page

--- src/components/ReportPaymentTable.tsx
+++ src/components/ReportPaymentTable.tsx
@@ -8,9 +8,9 @@ const ReportPaymentTable: React.FC<IProps> = ({ reports }) => {
   const owners = reports.owners;
   return (
     <div className="flex flex-col bg-white rounded-xl w-full p-5">
-      <div className="flex rounded-xl overflow-hidden shadow-xl">
-        <div className="w-full">
-          <table className="w-full text-sm text-left rtl:text-right text-gray-500 h-full">
+      <div className="flex rounded-xl shadow-xl overflow-auto">
+        <div className="w-full max-h-[500px]">
+          <table className="w-full text-left rtl:text-right text-gray-500 text-xs">
             <thead>
               <tr className="bg-slate-800 text-gray-400 text-center">
                 <th scope="col" className="px-6 py-3">
@@ -34,30 +34,34 @@ const ReportPaymentTable: React.FC<IProps> = ({ reports }) => {
                     {i === 0 && (
                       <>
                         <td
-                          className="px-6 py-4 font-medium text-gray-900"
+                          className="px-6 py-4 text-gray-900 border-gray-200 border-l-2 border-b-2"
                           rowSpan={cards.length}
                         >
                           {ownerId}
                         </td>
                         <td
-                          className="px-6 py-4  border-gray-200 border-x-2"
+                          className="px-6 py-4 border-gray-200 border-b-2"
                           rowSpan={cards.length}
                         >
                           {ownerName + " - " + total}
                         </td>
                       </>
                     )}
-                    <td className="px-6 py-4  border-gray-200 border-l-2">
+                    <td className="px-6 py-4  border-gray-200 border-x-2 border-b-2">
                       {c.title}
                     </td>
-                    <td>{c.total}</td>
+                    <td className="px-6 py-4 border-gray-200 border-b-2">
+                      {c.total}
+                    </td>
                   </tr>
                 ));
               })}
               <tr>
                 <td className="px-6 py-4"></td>
                 <td className="px-6 py-4"></td>
-                <td className="px-6 py-4">جمــــــــع:</td>
+                <td className="px-6 py-4  border-gray-200 border-l-2 font-black">
+                  جمــــــــع:
+                </td>
                 <td className="px-6 py-4">{reports.total}</td>
               </tr>
             </tbody>
